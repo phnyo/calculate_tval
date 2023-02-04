24ab985b2d76d6c7cf9a9d7932a81ccb1bf12f95
nyaos <sabanium@gmail.com>
2023-02-04 13:28:43 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### T値計算用プログラム
+
+PH科用になってるけど適当に書き換えるとISでもできる
+一般教養を除いているけど書き換えると対応可能です
+
+手順:
+
+1. CLASSからダウンロードした成績のpdfを同じディレクトリに入れる
+2. `pip install -r requirements.txt'
+3. `python3 calculate_tval.py`
+
+環境は`python3.8.16`, `pip 22.0.4`です
---
+++ calculate_tval.py
@@ -0,0 +1,37 @@
+import PyPDF2
+import glob
+import re
+
+text = ''
+
+with open(glob.glob("*.pdf")[0], "rb") as f:
+    reader = PyPDF2.PdfReader(f)
+    for i in range(len(reader.pages)):
+        text += reader.pages[i].extract_text()
+
+text_list = text.split('\n')
+t_val = 0
+
+PH_TVAL = [4, 3, 2, 1, 0]
+IS_TVAL = [7, 6, 3, 1, 0]
+
+for text in text_list:
+    if re.match('【.*】', text):
+        genre = text
+    if re.match('\s+[1-9]+\.0?\s+[Ａ|Ｂ|Ｃ|Ｄ|Ｓ]', text):
+        if re.match('.*[一般教養|初習|自由].*', genre):
+            continue
+        split_text = re.split('\s+', text)
+        if split_text[2] == 'Ｓ':
+            t_val += PH_TVAL[0] * float(split_text[1])
+        elif split_text[2] == 'Ａ':
+            t_val += PH_TVAL[1] * float(split_text[1])
+        elif split_text[2] == 'Ｂ':
+            t_val += PH_TVAL[2] * float(split_text[1])
+        elif split_text[2] == 'Ｃ':
+            t_val += PH_TVAL[3] * float(split_text[1])
+        else:
+            t_val += PH_TVAL[4] * float(split_text[1])
+        
+print(t_val)
+
