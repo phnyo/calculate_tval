b9c61aabc468550399409de4c9be88904a04ef77
nyaos <sabanium@gmail.com>
2023-02-04 14:32:48 +0900

    必修科目の重み付け

--- calculate_tval.py
+++ calculate_tval.py
@@ -16,22 +16,32 @@ PH_TVAL = [4, 3, 2, 1, 0]
 IS_TVAL = [7, 6, 3, 1, 0]
 
 for text in text_list:
+
     if re.match('【.*】', text):
         genre = text
+
     if re.match('\s+[1-9]+\.0?\s+[Ａ|Ｂ|Ｃ|Ｄ|Ｓ]', text):
+
         if re.match('.*[一般教養|初習|自由].*', genre):
             continue
-        split_text = re.split('\s+', text)
-        if split_text[2] == 'Ｓ':
-            t_val += PH_TVAL[0] * float(split_text[1])
-        elif split_text[2] == 'Ａ':
-            t_val += PH_TVAL[1] * float(split_text[1])
-        elif split_text[2] == 'Ｂ':
-            t_val += PH_TVAL[2] * float(split_text[1])
-        elif split_text[2] == 'Ｃ':
-            t_val += PH_TVAL[3] * float(split_text[1])
+
+        if re.match('.*[必修|基礎].*', genre):
+            comp_mul = 1.5
         else:
-            t_val += PH_TVAL[4] * float(split_text[1])
+            comp_mul = 1.0
+
+        split_text = re.split('\s+', text)
+        
+        if split_text[2] == 'Ｓ':
+            t_val += PH_TVAL[0] * float(split_text[1]) * comp_mul
+        elif split_text[2] == 'Ａ':
+            t_val += PH_TVAL[1] * float(split_text[1]) * comp_mul
+        elif split_text[2] == 'Ｂ':
+            t_val += PH_TVAL[2] * float(split_text[1]) * comp_mul
+        elif split_text[2] == 'Ｃ':
+            t_val += PH_TVAL[3] * float(split_text[1]) * comp_mul
+        else:
+            t_val += PH_TVAL[4] * float(split_text[1]) * comp_mul
         
 print(t_val)
 
